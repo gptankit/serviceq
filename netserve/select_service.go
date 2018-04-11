0d2e3b397db4a7943ca27e16ae3e31166ecfc1a7
Ankit Gupta <ankit.gupta@redbus.com>
2018-04-11 16:31:40 +0530

    _test for index bounds and correctness of select service algorithm

--- netserve/select_service.go
+++ netserve/select_service.go
@@ -6,6 +6,10 @@ import (
 
 func ChooseServiceIndex(noOfServices int, initialChoice int, retry int) int {
 
+	if noOfServices <= 0 {
+		return noOfServices
+	}
+
 	if retry == 0 { // first time
 		return algorithm.Randomize(noOfServices)
 	} else {
