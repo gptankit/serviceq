3a19e37415c5d346c48cd4e904d418d042f657dd
Ankit Gupta <ankit.gupta@redbus.com>
2018-04-12 23:56:14 +0530

    Adjusted orchestration test params

--- serviceq_test.go
+++ serviceq_test.go
@@ -12,12 +12,10 @@ func TestOrchestrationStates(t *testing.T) {
 
 	// assumption -- all services are down
 
-	// assign initial values for cconn, (use bufio.Writer on conn), creq, cwork, sqprops
-
 	sqp := model.ServiceQProperties{}
 	sqp.Proto = "http"
 	sqp.ServiceList = []string{"http://example.org:2001", "http://example.org:3001", "http://example.org:4001", "http://example.org:5001"}
-	sqp.MaxConcurrency = 64 // if changing, do check value of duplicateWork
+	sqp.MaxConcurrency = 8 // if changing, do check value of duplicateWork
 	sqp.MaxRetries = 1      // we know it's down
 	sqp.RetryGap = 1000     // ms
 	sqp.IdleGap = 500       // ms
@@ -33,35 +31,34 @@ func TestOrchestrationStates(t *testing.T) {
 	cr <- req
 	cw <- 1
 
-	// call go orchestrate and note counts in cconn, creq
 	go orchestrate(cc, cr, cw, &sqp) // this will start executing req
 
 	// increment/decrement buffer (+1/-1) in creq, cwork and give time to orchestrate
 
 	duplicateWork := int(sqp.MaxConcurrency/2) + 1
 
-	time.Sleep(2 * time.Second)
+	time.Sleep(1000 * time.Millisecond)
+	// add req and work again
 	for i := 0; i < duplicateWork; i++ {
-		// add req and work again
 		cr <- req
 		cw <- 1
 	}
 
-	time.Sleep(2 * time.Second)
+	time.Sleep(1000 * time.Millisecond)
+	// remove all work without removing req
 	for i := 0; i < duplicateWork+1; i++ {
-		// remove all work without removing req
 		<-cw
 	}
 
-	time.Sleep(5 * time.Second)
+	time.Sleep(1000 * time.Millisecond)
+	// add half of works
 	for i := 0; i < ((duplicateWork + 1) / 2); i++ {
-		// add half of works
 		cw <- 1
 	}
 
-	time.Sleep(5 * time.Second)
+	time.Sleep(3000 * time.Millisecond)
 
 	if len(cw) < ((duplicateWork+1)/2)-1 || len(cw) > ((duplicateWork+1)/2) {
-		t.Errorf("Work not being orchestrated properly")
+		t.Errorf("Work not being orchestrated properly\n")
 	}
 }
